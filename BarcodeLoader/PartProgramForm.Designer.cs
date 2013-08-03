e44c0da6a4e96cb54da38e30a1236d2ffb7f4af8
Phil Coltrane <phil.m.coltrane@gmail.com>
2013-08-03 08:49:34 -0400

    Started laying out part program editing form
    Form to be used for adding or editing part program.
    Needs UI elements.
    Use OpenFileDialog to select files.
    Add a PartProgram property to set part program for editing, and get part
    program after exit.

---
+++ BarcodeLoader/PartProgramForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BarcodeLoader
+{
+    partial class PartProgramForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BarcodeTextBox = new System.Windows.Forms.TextBox();
+            this.FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProgramLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Barcode (hint: select this box and scan the barcode):";
+            // 
+            // BarcodeTextBox
+            // 
+            this.BarcodeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.BarcodeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BarcodeTextBox.Location = new System.Drawing.Point(15, 25);
+            this.BarcodeTextBox.Name = "BarcodeTextBox";
+            this.BarcodeTextBox.Size = new System.Drawing.Size(597, 38);
+            this.BarcodeTextBox.TabIndex = 2;
+            // 
+            // FileDialog
+            // 
+            this.FileDialog.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Program:";
+            // 
+            // ProgramLabel
+            // 
+            this.ProgramLabel.AutoSize = true;
+            this.ProgramLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgramLabel.Location = new System.Drawing.Point(12, 119);
+            this.ProgramLabel.Name = "ProgramLabel";
+            this.ProgramLabel.Size = new System.Drawing.Size(75, 20);
+            this.ProgramLabel.TabIndex = 4;
+            this.ProgramLabel.Text = "ABC.MIN";
+            // 
+            // PartProgramForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 361);
+            this.Controls.Add(this.ProgramLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BarcodeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "PartProgramForm";
+            this.Text = "PartProgramForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BarcodeTextBox;
+        private System.Windows.Forms.OpenFileDialog FileDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ProgramLabel;
+    }
+}
